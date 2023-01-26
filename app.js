552a4230553293b6233af2de04d3e3f06ac7a35f
SCIENCE\senkovenkod <d.senkovenko@elsevier.com>
2023-01-26 01:03:14 -0800

    conf

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var log = function(entry) {
+  fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
 
-var app = express();
+var server = http.createServer(function (req, res) {
+  if (req.method === 'POST') {
+    var body = '';
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+    req.on('data', function(chunk) {
+      body += chunk;
+    });
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    req.on('end', function() {
+      if (req.url === '/') {
+        log('Received message: ' + body);
+      } else if (req.url = '/scheduled') {
+        log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+      }
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+      res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+      res.end();
+    });
+  } else {
+    res.writeHead(200);
+    res.write(html);
+    res.end();
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
